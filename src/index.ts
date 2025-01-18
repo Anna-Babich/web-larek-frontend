50b2da086b034d13b0b2bd63fd2eb1bee3c0b924
Anna-Babich <anbabi@yandex.ru>
2025-01-18 17:24:41 +0300

    Первый коммит

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
 import './scss/styles.scss';
+
+interface IProduct {
+    _id: string;
+    description: string;
+    image: string;
+    title: string;
+    category: string;
+    price: number | null;
+    selected?: boolean;
+}
+
+
+interface IUserInfo {
+    payment: string;
+    email: string;
+    phone: string;
+    address: string;
+    total: number;
+    items: string[];
+}
+
+interface IProductsData {
+    products: IProduct[];
+    preview: string | null;
+}
+
+type TProductInfo = Pick<IProduct, 'description' | 'image' | 'title' | 'category' | 'price'>;
+type TBasket = Pick<IProduct, 'title' | 'price'>;
+type TFormPayment = Pick<IUserInfo, 'payment' | 'address'>;
+type TFormContact = Pick<IUserInfo, 'email' | 'phone'>;
